476f3252980b1ba8d39948b0eb09f2b8ece49235
Alexander Kostarev <UniverAlex@ya.ru>
2017-12-24 17:52:26 +0300

    if applied, this commit will put list_store in Model and add working insert

--- sportsmen_mocker.py
+++ sportsmen_mocker.py
@@ -1,5 +1,3 @@
-from operator import itemgetter
-
 from sport_statistic_lib.sportsman import *
 
 
@@ -10,9 +8,3 @@ class SportsmenMocker():
     def execute(self):
         self.sportsmen_list.append(Sportsman("Иванов", "Ш", 105))
         self.sportsmen_list.append(Sportsman("Петров", "A", 106.3))
-        self.add_place()
-
-    def add_place(self):
-        self.sportsmen_list.sort(key=itemgetter(3))
-        for i in range(0, len(self.sportsmen_list)):
-            self.sportsmen_list[i].place = i + 1
