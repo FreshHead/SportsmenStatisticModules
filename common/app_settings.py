7c0eb0fe5e9b79283599d9ebfab1b2a30f4c6f23
Alexander Kostarev <UniverAlex@ya.ru>
2018-01-04 13:47:48 +0300

    if applied, this commit will define logging settings in yaml configuration file

---
+++ common/app_settings.py
@@ -0,0 +1,10 @@
+import yaml
+
+
+class AppSettings:
+    def __init__(self):
+        with open('config.yml', 'rt') as fin:
+            text = fin.read()
+        data = yaml.safe_load(text)
+        self.log_filename = data['logging']['filename']
+        self.log_level = data['logging']['level']
--- utility/log_utility.py
+++ utility/log_utility.py
@@ -1,6 +1,6 @@
 import logging
 
 def create_logger(level='DEBUG', filename='app.log'):
-    fmt = '%(asctime)s %(levelname)s %(lineno)s %(message)s'
+    fmt = '%(asctime)s %(levelname)s %(lineno)s: %(message)s'
     logging.basicConfig(level=logging._nameToLevel[level], filename=filename, format=fmt)
     return logging.getLogger()
